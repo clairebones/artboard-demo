73be35e4131ebbe7e0ef6313bebad83c6b1f3116
Claire Wilgar <cwilgar@gmail.com>
2020-03-03 21:46:55 +0000

    Add assets and implement artboard header

--- src/Artboard.js
+++ src/Artboard.js
@@ -2,8 +2,13 @@ import React, { Fragment } from 'react';
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import './Artboard.scss';
+import closeIcon from './assets/close.svg';
+import breadcrumbIcon from './assets/breadcrumb.svg';
+import arrowLeftIcon from './assets/arrow-left.svg';
+import arrowRightIcon from './assets/arrow-right.svg';
 
 const Artboard = () => {
   let { id } = useParams();
@@ -47,9 +52,33 @@ const Artboard = () => {
   });
 
   return (
-    <Fragment className="artboard">
+    <Fragment>
       <header className="artboard_header">
-        {artboard.name}
+        <div className="artboard_header--nav">
+          <div className="closeButton">
+            <Link to={`/`}><img className="closeIcon" src={closeIcon}/></Link>
+          </div>
+          <div className="nav">
+            {(id > 0) &&
+              <Link to={`/`+(parseInt(id)-1)}>
+                <img className="navIcon" src={arrowLeftIcon}/>
+              </Link>
+            }
+              <div className="breadcrumbs">
+                {parseInt(id)+1}
+                <img className="navIcon" src={breadcrumbIcon}/>
+                {data.share.version.document.artboards.entries.length}
+              </div>
+              {(id < (data.share.version.document.artboards.entries.length-1)) &&
+                <Link to={`/`+(parseInt(id)+1)}>
+                  <img className="navIcon" src={arrowRightIcon}/>
+                </Link>
+              }
+            </div>
+          </div>
+        <div className="artboard_header--name">
+          <span>{artboard.name}</span>
+        </div>
       </header>
       <main className="artboard_container">
         <img 
